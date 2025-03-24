5be038b61d1385707151c43dcceac6a8c30c9e6c
maqsud1987 <maqsudsulaymonov86@gmail.com>
2025-03-24 11:20:18 +0500

    O'zgaruvchilar

---
+++ JS_1-DARS/js/main.js
@@ -0,0 +1,26 @@
+/*var number_1 =20;
+var number_2 =50;
+if(number_1 == 20 && number_2 > 50){
+    console.log("OK");
+}else if(number_1 > 15 && number_2 > 50){
+    console.log("OK!");
+}else if(number_1 >=20 && number_2 < 60){
+    console.log("True")
+} else {
+    console.log("Error");
+}*/
+var line = "Word"
+switch(line){
+    case"8":
+    console.log("O'zgaruvchi 8")
+    break;
+    case"4":
+    console.log("O'zgaruvchi ")
+    break;
+    case"Word":
+    console.log("O'zgaruvchi <<Word>>")
+    break;
+    default:
+    console.log("Error")
+
+}
