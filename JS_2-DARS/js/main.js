dcb06e08e9614230b89a2db2ef6bbbee24879c07
maqsud1987 <maqsudsulaymonov86@gmail.com>
2025-03-25 18:34:30 +0500

    List bilan ishlash

---
+++ JS_2-DARS/js/main.js
@@ -0,0 +1,11 @@
+//одномерные массивы
+var arr = [5,21,0.2,-5,100,"True"]
+arr[3] = "Word";
+console.log(arr);
+
+//многомерные массивы
+var matrix = [
+    [4,6,8],['stroka',5.7],[0,-100]
+]
+matrix [0] [2] = -500
+console.log(matrix[1][0])
